aaf5c47b0c3e14356c655e716fca445f97fcd498
孙茂胤 <simonmysun@gmail.com>
2013-12-21 08:09:18 +0800

    init

---
+++ js/decoration.js
@@ -0,0 +1,12 @@
+document.onreadystatechange = function(){
+    if(document.readyState=="complete"){
+	$('#loading').fadeOut(2000,function(){
+	    $('#merrychristmas').fadeIn(3000,function(){
+		$('#sincerely').fadeIn(1000);
+		$('#bycoyouth').fadeIn(3000,function(){
+		    
+		});
+	    });
+	});
+    }
+}
