a68f33a0312a02adfa1d8ae025126f5a478b1cb4
孙茂胤 <simonmysun@gmail.com>
2013-12-25 18:51:36 +0800

    style

--- fest/christmas/js/decoration.js
+++ fest/christmas/js/decoration.js
@@ -1,3 +1,9 @@
+var head = document.getElementsByTagName('HEAD').item(0);
+var style = document.createElement('link');
+style.href = 'css/style.css';
+style.rel = 'stylesheet';
+style.type = 'text/css';
+head.appendChild(style);
 document.onreadystatechange = function(){
     if(document.readyState=="complete"){
 	$('#loading').fadeOut(2000,function(){
