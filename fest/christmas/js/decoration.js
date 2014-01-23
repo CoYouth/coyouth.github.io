0ce8fa2bb629383ca878fac04ce098920e049624
孙茂胤 <simonmysun@gmail.com>
2014-01-23 16:07:43 +0800

    failed

--- fest/christmas/js/decoration.js
+++ fest/christmas/js/decoration.js
@@ -4,14 +4,15 @@ style.href = 'css/style.css';
 style.rel = 'stylesheet';
 style.type = 'text/css';
 head.appendChild(style);
-window.onload = function(){
-    $("audio")[0].play();
-    $('#loading').fadeOut(2000,function(){
-	$('#merrychristmas').fadeIn(3000,function(){
-	    $('#sincerely').fadeIn(1000);
-	    $('#bycoyouth').fadeIn(3000,function(){
-		$(this).addClass('bigbigbig');
-	    });
-	});
-    });
+document.onreadystatechange = function(){
+    if(document.readyState=="complete"){
+        $('#loading').fadeOut(2000,function(){
+            $('#merrychristmas').fadeIn(3000,function(){
+                $('#sincerely').fadeIn(1000);
+                $('#bycoyouth').fadeIn(3000,function(){
+                    $(this).addClass('bigbigbig');
+                });
+            });
+        });
+    }
 }
